32bb7aadf2cebb16fddc1468f4077ca64ea291fb
Phillip Dudley <Predatorian3@gmail.com>
2016-02-26 21:33:25 -0600

    Initial Commit

---
+++ run_chef_verify.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#
+# Runs `chef verify` which will fail, but the script will work.
+#
+
+chef verify
+exit 0 
---
+++ run_docker_container.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#
+#
+#
+
+docker build -t centos_chefdk .
+
+#docker -i -t --name chefdk_test centos_chefdk /bin/bash 
