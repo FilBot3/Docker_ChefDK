ca520e38e92ffeb0ed890a473c5a0e7ad386ee60
Phillip Dudley <Predatorian3@gmail.com>
2016-03-02 21:25:45 -0600

    Changed shell scripts.

---
+++ run_ChefDK_conatiner.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#
+#
+#
+
+docker start ChefDK
+docker exec -i -t ChefDK /bin/bash
+
--- run_docker_container.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#
-#
-#
-
-docker build -t centos_chefdk .
-
-#docker -i -t --name chefdk_test centos_chefdk /bin/bash 
